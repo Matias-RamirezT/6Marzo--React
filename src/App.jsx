fcf092fa12d858cb3ce1846fb70daefbd4db8cc4
Matias-RamirezT <matias.ramirezt@uniagustiniana.edu.co>
2026-03-06 19:13:51 +0000

    Componentes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Estado from './estado'
+import String from './string';
+import Booleano from './booleano';
+import Lista from './lista';
+import Objeto from './objeto';
+
+export default function App() {
+  return (
+    <>
+    <Booleano/>
+    <Estado/>
+    <String/>
+    <Lista/>
+    <Objeto/>
+    </>
+  );
+}
